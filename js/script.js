5554f8b4b224d715bfa3dd8397b7dd91726cd694
AndyDAntuono <andrea.dantuono88@gmail.com>
2024-07-21 17:26:40 +0200

    [UPD] Milestone 1 completed

--- js/script.js
+++ js/script.js
@@ -1,17 +1,63 @@
 const {createApp} = Vue;
 
-createApp ({
+createApp({
     data() {
         return {
             contacts: [
                 {
                     name: 'Michele',
                     avatar: './img/avatar_1.jpg',
-                    //la sta per "last acess"
+                    // la sta per "last access"
                     laText: 'Ultimo messaggio inviato',
                     laHours: '12:00',
-                }
-            ]
-        }
+                },
+                {
+                    name: 'Fabio',
+                    avatar: './img/avatar_2.jpg',
+                    // la sta per "last access"
+                    laText: 'Ultimo messaggio inviato',
+                    laHours: '15:30',
+                },
+                {
+                    name: 'Samuele',
+                    avatar: './img/avatar_3.jpg',
+                    // la sta per "last access"
+                    laText: 'Ultimo messaggio inviato',
+                    laHours: '15:30',
+                },
+                {
+                    name: 'Alessandro B.',
+                    avatar: './img/avatar_4.jpg',
+                    // la sta per "last access"
+                    laText: 'Ultimo messaggio inviato',
+                    laHours: '15:30',
+                },
+                {
+                    name: 'Alessandro L.',
+                    avatar: './img/avatar_5.jpg',
+                    // la sta per "last access"
+                    laText: 'Ultimo messaggio inviato',
+                    laHours: '15:30',
+                },
+                {
+                    name: 'Claudia',
+                    avatar: './img/avatar_6.jpg',
+                    // la sta per "last access"
+                    laText: 'Ultimo messaggio inviato',
+                    laHours: '15:30',
+                },
+                {
+                    name: 'Federico',
+                    avatar: './img/avatar_7.jpg',
+                    // la sta per "last access"
+                    laText: 'Ultimo messaggio inviato',
+                    laHours: '15:30',
+                },
+                
+            ],
+        };
+    },
+    mounted() {
+        console.log(this.contacts);
     }
 }).mount('#app');
